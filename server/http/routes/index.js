fae0893b342e6c9af21e3214a46837715423b8ad
seay98 <haiyu851@163.com>
2020-08-22 17:45:24 +0800

    save post info

--- server/http/routes/index.js
+++ server/http/routes/index.js
@@ -29,54 +29,45 @@ router.post('/moien', function (req, res, next) {
         // console.log('Connected to the clients database.');
     });
 
-    let cid = 0;
-    let sql = '';
-    db.run(
-        `CREATE TABLE IF NOT EXISTS clients (
+    db.serialize(() => {
+        db.run(
+            `CREATE TABLE IF NOT EXISTS clients (
                     cid INTEGER PRIMARY KEY,
                     hostname text,
                     ip text,
                     os text,
                     utime text)`
-    );
-
-    sql = `SELECT cid, ip, os, utime FROM clients WHERE ip=?`;
-    db.get(sql, [ip], (err, row) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        if (row) {
-            cid = row.cid;
-            console.log(cid + 'in');
-        }
-    });
-    console.log(cid);
-    if (cid == 0) {
-        sql = `INSERT INTO clients (hostname, ip, os, utime) VALUES(?, ?, ?, datetime('now', 'localtime'))`;
-        db.run(sql, [req.body.hostname, ip, req.body.os], function (err) {
+        ).get(`SELECT cid, ip, os, utime FROM clients WHERE ip=?`, [ip], (err, row) => {
             if (err) {
-                return console.log(err.message);
+                return console.error(err.message);
             }
-            // get the last insert id
-            console.log(`A row has been inserted with rowid ${this.lastID}`);
-        });
-    } else {
-        sql = `UPDATE clients SET hostname=?, ip=?, os=?, utime=datetime('now', 'localtime') where cid=?`;
-        db.run(sql, [req.body.hostname, ip, req.body.os, cid], function (err) {
-            if (err) {
-                return console.log(err.message);
+            if (row) {
+                let cid = row.cid;
+                let sql = `UPDATE clients SET hostname=?, ip=?, os=?, utime=datetime('now', 'localtime') where cid=?`;
+                db.run(sql, [req.body.hostname, ip, req.body.os, cid], function (err) {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                    console.log(`Row(s) updated: ${this.changes}`);
+                });
+            } else {
+                let sql = `INSERT INTO clients (hostname, ip, os, utime) VALUES(?, ?, ?, datetime('now', 'localtime'))`;
+                db.run(sql, [req.body.hostname, ip, req.body.os], function (err) {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                    // get the last insert id
+                    console.log(`A row has been inserted with rowid ${this.lastID}`);
+                });
             }
-            console.log(`Row(s) updated: ${this.changes}`);
+            db.close((err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                // console.log('Close the database connection.');
+            });
         });
-    }
-    
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        // console.log('Close the database connection.');
     });
 });
-
 
 module.exports = router;
